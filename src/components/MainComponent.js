7ad4bdfa50c22d4f3f27ca759aa743163422a15f
yohannes444 <yohannesmulat444@gmail.com>
2022-11-29 21:48:16 +0300

    single page part one

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,8 @@
 import Menu from "./MenuComponent"
 import { DISHES } from "../shared/dishes"
+/* import { LEADERS } from "../shared/leaders"
+import { COMMENTS } from "../shared/comments"
+import { PROMOTIONS } from "../shared/promotiones" */
 import React,{ Component } from 'react';
 //import  DishdetailComponent  from "./DishdetailComponent"
 import Header from './headerComponent';
@@ -8,49 +11,39 @@ import Home from './HomeComponent'
 import { Routes, Route } from "react-router-dom";
 import Contactus from './contactComponent'
 
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotiones';
+import { LEADERS } from '../shared/leaders';
 
 
 class  Main extends Component {
-  constructor(props){
-    super(props)
-    this.state={
-      dishes:DISHES,
-      selectedDish:null,
-      selectOne:false,
-      comment:null
-    }
-  };
-  onDishSelecte(dishId){
-        
-    this.setState({selectedDish: dishId ,  selectOne: true ,   comment:dishId.comments })
-  
-}
-onSetAfter(empty){
-  this.setState({selectedDish: empty.selectedDish, selectOne:empty.seletedOne, comment:empty.comment})
-}
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          dishes: DISHES,
+           
+        };
+      }
+
   render(){
     function HomePage(){
       return(
-        <Home/>
+        <Home dish={DISHES.filter((dish) => dish.featured)[0]}
+        promotion={PROMOTIONS.filter((promo) => promo.featured)[0]}
+        leader={LEADERS.filter((leader) => leader.featured)[0]}
+          />
       )
     }
    
     return (
 
       <div className="App">
-        
-        
-        {/* 
-        <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelecte(dishId)} setAfter={(empty)=>this.onSetAfter(empty)}/>
-        {this.state.selectOne &&(<DishdetailComponent   comments={this.state.comment} dish={this.state.dishes.filter((dish)=>dish.id === this.state.selectedDish)[0]}/>)}
-         */}
             
          <Header/>
-     
-          
+
          <Routes>
-          
-          <Route path='/home'  element={<HomePage/>} />
+         <Route path='/home'  element={<HomePage/>} />
           <Route exact path='/menu' element={ <Menu dishes={this.state.dishes} />} />
           <Route exact path='/contactus' element={<Contactus />}/>
           
